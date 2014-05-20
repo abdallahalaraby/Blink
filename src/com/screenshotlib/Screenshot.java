9b5592f0a6ed4543d7172978d6d7ad9e3c85c3e5
Abdallah Alaraby <abdallah.alaraby@nabdacare.com>
2014-05-20 16:40:12 +0200

    first commit

---
+++ src/com/screenshotlib/Screenshot.java
@@ -0,0 +1,42 @@
+package com.screenshotlib;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.View.MeasureSpec;
+
+public class Screenshot {
+
+	public static void takeScreenshot(View view, String filePath) {
+		Bitmap bitmap = getBitmapScreenshot(view, filePath);
+
+		File imageFile = new File(filePath);
+		try {
+			OutputStream fout = new FileOutputStream(imageFile);
+			bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fout);
+			fout.flush();
+			fout.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Bitmap getBitmapScreenshot(View view, String filePath) {
+		view.measure(MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED), MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+		view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+
+		view.setDrawingCacheEnabled(true);
+		view.buildDrawingCache(true);
+		Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+		view.setDrawingCacheEnabled(false);
+
+		return bitmap;
+	}
+}
